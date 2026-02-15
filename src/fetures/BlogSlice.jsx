63a52a79a5c31944faba385ab37bb204f8afbfbd
satyam-yadav12 <satyam.dev637@gmail.com>
2026-02-15 14:47:23 +0530

    added little polish in css

--- src/fetures/BlogSlice.jsx
+++ src/fetures/BlogSlice.jsx
@@ -1,61 +1,51 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-export const fetchBlogs = createAsyncThunk(
-    "bolgs/fetchBlogs", async () => {
-        const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+export const fetchBlogs = createAsyncThunk("bolgs/fetchBlogs", async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
 
-        if(!response.ok){
-            throw new Error("unable to fetch blogs")
-        }
-        return await response.json()
-    }
-        
- 
-)
+  if (!response.ok) {
+    throw new Error("unable to fetch blogs");
+  }
+  return await response.json();
+});
 
 const initialState = {
-    blogs: [],
-    currentPage: 1,
-    loading: false,
-    error: null
-}
+  blogs: [],
+  currentPage: 1,
+  loading: false,
+  error: null,
+};
 
 const BlogSlice = createSlice({
-    name: "blogs", 
-    initialState,
-    reducers: 
-    {
-        removeBlog: (state, action)=>{
-            let newBlogs = state.blogs.filter((value)=>value.id != action.payload)
-        
-            state.blogs = newBlogs
-          
-        },
-        moveToPage: (state, action)=>{
-            state.currentPage = action.payload
-    }
-  
+  name: "blogs",
+  initialState,
+  reducers: {
+    removeBlog: (state, action) => {
+      let newBlogs = state.blogs.filter((value) => value.id != action.payload);
 
-},
-    extraReducers: (builder)=>{
-        builder
-        .addCase(fetchBlogs.pending, (state)=>{
-            state.loading = true
-            state.error = null
-        })
-        .addCase(fetchBlogs.fulfilled, (state, action)=>{
-            state.blogs = action.payload 
-            state.loading = false
-            state.error = null
-           
-        })
-        .addCase(fetchBlogs.rejected, (state, action)=>{
-            state.loading = false
-            state.error = action.error.message
-        })
-    }
+      state.blogs = newBlogs;
+    },
+    moveToPage: (state, action) => {
+      state.currentPage = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchBlogs.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchBlogs.fulfilled, (state, action) => {
+        state.blogs = action.payload;
+        state.loading = false;
+        state.error = null;
+      })
+      .addCase(fetchBlogs.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
+});
 
-})
-
-export const {removeBlog, moveToPage} = BlogSlice.actions
-export default BlogSlice.reducer
+export const { removeBlog, moveToPage } = BlogSlice.actions;
+export default BlogSlice.reducer;
